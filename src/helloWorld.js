e8a3c16f2a9625c33229e4bebf5833b61a822210
Rebecca L Meents <rmeents91@gmail.com>
2021-05-06 15:10:09 -0700

    added cors

--- src/helloWorld.js
+++ src/helloWorld.js
@@ -1,9 +1,13 @@
 const express = require('express');
+const cors = require('cors');
+
 const app = express();
 const port = 3000;
 
+app.use(cors());
+
 app.get('/hello-world', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World from the api!');
 });
 
-app.listen(port, () => console.log(`hello from port ${port}!`))
+app.listen(port, () => console.log(`hello from port ${port}!`));
